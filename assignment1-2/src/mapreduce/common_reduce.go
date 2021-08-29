358659378a0d8b637d21af8183cd85c390e90776
rodlanand <rodlanand@outlook.com>
2021-08-28 19:00:19 -0500

    Solves assignment1-2 (1).

--- assignment1-2/src/mapreduce/common_reduce.go
+++ assignment1-2/src/mapreduce/common_reduce.go
@@ -1,5 +1,10 @@
 package mapreduce
 
+import (
+	"encoding/json"
+	"os"
+)
+
 // doReduce does the job of a reduce worker: it reads the intermediate
 // key/value pairs (produced by the map phase) for this task, sorts the
 // intermediate key/value pairs by key, calls the user-defined reduce function
@@ -10,27 +15,34 @@ func doReduce(
 	nMap int, // the number of map tasks that were run ("M" in the paper)
 	reduceF func(key string, values []string) string,
 ) {
-	// TODO:
-	// You will need to write this function.
-	// You can find the intermediate file for this reduce task from map task number
-	// m using reduceName(jobName, m, reduceTaskNumber).
-	// Remember that you've encoded the values in the intermediate files, so you
-	// will need to decode them. If you chose to use JSON, you can read out
-	// multiple decoded values by creating a decoder, and then repeatedly calling
-	// .Decode() on it until Decode() returns an error.
-	//
-	// You should write the reduced output in as JSON encoded KeyValue
-	// objects to a file named mergeName(jobName, reduceTaskNumber). We require
-	// you to use JSON here because that is what the merger than combines the
-	// output from all the reduce tasks expects. There is nothing "special" about
-	// JSON -- it is just the marshalling format we chose to use. It will look
-	// something like this:
-	//
-	// enc := json.NewEncoder(mergeFile)
-	// for key in ... {
-	// 	enc.Encode(KeyValue{key, reduceF(...)})
-	// }
-	// file.Close()
-	//
-	// Use checkError to handle errors.
+	keyValues := []KeyValue{}
+	for m := 0; m < nMap; m++ {
+		rFile, err := os.Open(reduceName(jobName, m, reduceTaskNumber))
+		checkError(err)
+
+		dec := json.NewDecoder(rFile)
+		for {
+			var kv KeyValue
+			err := dec.Decode(&kv)
+			if err != nil {
+				break
+			}
+			keyValues = append(keyValues, kv)
+		}
+		rFile.Close()
+	}
+
+	keyValuesMap := map[string][]string{}
+	for _, kv := range keyValues {
+		keyValuesMap[kv.Key] = append(keyValuesMap[kv.Key], kv.Value)
+	}
+
+	mergeFile, err := os.Create(mergeName(jobName, reduceTaskNumber))
+	checkError(err)
+
+	enc := json.NewEncoder(mergeFile)
+	for key, values := range keyValuesMap {
+		checkError(enc.Encode(KeyValue{key, reduceF(key, values)}))
+	}
+	mergeFile.Close()
 }
